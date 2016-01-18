9bc4c2803758f54c657b17806d002b96b5b3c0be
Bob van Schendel <bobyantius@hotmail.com>
2016-01-18 11:02:15 +0100

    first commit, sfml runs, now starting to try some things out to get a feel for it and then start adding game logic.

---
+++ SFMLTronCoroutine/Program.fs
@@ -0,0 +1,17 @@
+﻿open SFML.Window
+open SFML.Graphics
+
+let mainWindow = new RenderWindow(new VideoMode(600ul, 600ul), "Empty")
+mainWindow.SetFramerateLimit(40ul);
+mainWindow.Closed.AddHandler(fun sender args -> (sender :?> RenderWindow).Close())
+
+let rec mainLoop() =
+  mainWindow.Clear()
+  mainWindow.DispatchEvents()
+  mainWindow.Display()
+
+  match mainWindow.IsOpen with
+  | true -> mainLoop()
+  | false -> ()
+
+mainLoop()
